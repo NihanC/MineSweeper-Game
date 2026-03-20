1121ae29d3d492e45a5d60d8fedfeb1ff0412c60
johannak32 <jmsarte37@gmail.com>
2026-03-20 12:13:08 +0100

    added a test class

---
+++ src/main/test/java/GameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.myteam.minesweeper.Game;
+import org.myteam.minesweeper.Level;
+
+public class GameTest {
+
+    @Test
+    public void test1() {
+        Game game = new Game(Level.EASY);
+        
+    }
+}
