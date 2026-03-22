f4a387700022d57e44bce1357828c17edd19fbec
johannak32 <jmsarte37@gmail.com>
2026-03-22 16:53:36 +0100

    added testMineAllocation method in GameTest

--- src/main/test/java/GameTest.java
+++ src/main/test/java/GameTest.java
@@ -4,6 +4,7 @@ import org.myteam.minesweeper.*;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class GameTest {
 
@@ -46,6 +47,30 @@ public class GameTest {
         Board hardBoard = new Board(Level.HARD);
         assertEquals(16, hardBoard.getRowNum());
         assertEquals(32, hardBoard.getColNum());
+    }
+
+    @Test
+    public void testMineAllocation() {
+        testMineAllocationForLevel(Level.EASY);
+        testMineAllocationForLevel(Level.MEDIUM);
+        testMineAllocationForLevel(Level.HARD);
+    }
+
+    public void testMineAllocationForLevel(Level level) {
+        Random rand = new Random();
+        for (int attempt = 0; attempt < 100; attempt++) {
+            Board board = new Board(level);
+            int firstRow = rand.nextInt(board.getRowNum());
+            int firstCol = rand.nextInt(board.getColNum());
+            board.click(false,firstRow,firstCol,new Game(level));
+            for (int i = firstRow - 1; i <= firstRow + 1; i++) {
+                for (int j = firstCol - 1; j <= firstCol + 1; j++) {
+                    if (i >= 0 && i < board.getRowNum() && j >= 0 && j < board.getColNum()) {
+                        assertFalse(board.getGrid()[i][j] instanceof Mine);
+                    }
+                }
+            }
+        }
     }
 
 }
